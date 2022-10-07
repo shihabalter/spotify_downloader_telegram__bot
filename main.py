6a6f87b9ebc9f4c2ac7abd6267079e0bfde7c617
Shihab <108454690+shihabalter@users.noreply.github.com>
2022-10-07 17:13:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ Name album - Name artist
 '''
 
 sort = {}
-telegram_token = 'token'
+telegram_token = '5794992891:AAHwtErUQsvJgxEQdNuHwYUSMiTZ9V60Ot4'
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text=WELCOME)
